abef161b2032d1b3ca18bf530946e551f8f75f4c
Ichooooooo <3490368487@qq.com>
2026-08-16 21:30:42 +0800

    add a tongpai.md

---
+++ CSES/Range Queries/2206.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+using arr2 = array <int, 2>;
+using arr3 = array <int, 3>;
+const int mod = 1e9 + 7;
+
+void ovo() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int _=1;
+    // cin>>_;
+
+    while (_--) ovo();
+}
